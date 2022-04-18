9fc30e9380fb500752fbad54ecfb8106b9e56980
NguyenDacTien2003 <103101867+NguyenDacTien2003@users.noreply.github.com>
2022-04-18 17:01:40 +0700

    up

---
+++ product/product.java
@@ -0,0 +1,14 @@
+public class product {
+    string name;
+    int age;
+    float price;
+    int quantity;
+    int id ;
+    public product (){
+        name = ("dactien");
+        price = (float) 8.13;
+        quantity = 4 ;
+        id =10;
+    }
+    public  product( string name)
+}
---
+++ product/src/produck.java
@@ -0,0 +1,28 @@
+public class produck {
+
+        String name;
+        float price;
+        int qty;
+        int id;
+
+        public Product(String Name,float Price,int Qty,int Id) {
+            name = Name;
+            price = Price;
+            qty = Qty;
+            id = Id;
+        }
+
+        public String getName(){
+            return name;
+        }
+        public float getPrice(){
+            return price;
+        }
+        public int getQty(){
+            return qty;
+        }
+        public int getId(){
+            return id;
+        }
+    }
+
