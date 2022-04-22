3ef2bc651c00c9771bbc3ff2b1d2991a8cecef1d
NguyenDacTien2003 <103101867+NguyenDacTien2003@users.noreply.github.com>
2022-04-22 16:25:20 +0700

    up

---
+++ Dactien2/src/time.java
@@ -0,0 +1,29 @@
+public class time {
+
+    private int hour;
+    private int minute;
+    private int second;
+
+    public time(int a, int b, int c) {
+        a = hour;
+        if ( a >= 0 && a <= 23) {
+        }
+
+        b = minute;
+        if ( b >= 0 && b <= 59){
+
+        }
+
+        c = second;
+        if (c >= 0 && c <=59){
+
+        }
+    }
+    public int getHour (){
+        return hour;
+    }
+
+
+
+}
+
